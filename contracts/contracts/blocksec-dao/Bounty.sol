01ba845a97c59cb6d0e6f1e30315a56d6d52c68a
epociask <empociask@dons.usfca.edu>
2023-01-22 20:27:08 -0800

    Fixing compilation issues

--- contracts/contracts/blocksec-dao/Bounty.sol
+++ contracts/contracts/blocksec-dao/Bounty.sol
@@ -11,7 +11,7 @@ library Core {
 
     struct SubmissionMetaData {
         address whiteHat;
-        uint256 vulnStorageID;
+        bytes32 vulnStorageID;
         uint256 timeStamp;
         SubmissionStatus status;
     }
@@ -47,6 +47,7 @@ contract Bounty is Ownable {
         // TODO - Validate metadata ID
         require(endTimeStamp_ > block.timestamp, "End timestamp must be greater than the start timestamp");
         require(maxSubmissions_ >= minSubmissions, "Max submissions should always be greater than minSubmissions");
+        require(bountyHost_ != address(0), "Bounty host cannot be NULL address");
 
         bountyHost = bountyHost_;
         tokenAddress = tokenAddress_;
@@ -77,7 +78,7 @@ contract Bounty is Ownable {
 
     // TODO - Make collatoral an ERC-20 token
     // ** Submission logic functionality **/   
-    function submitVulnerability(address beneficiary, uint256 storageID, uint256 collatoral) public returns (bytes32) {
+    function submitVulnerability(address beneficiary, bytes32 storageID, uint256 collatoral) public returns (bytes32) {
         require(submissionCount <= maxSubmissions-1, "Submission limit has currently been exceeded");
         require(beneficiary != bountyHost, "You cannot submit vulnerabilites as bounty host");
 
@@ -106,6 +107,16 @@ contract Bounty is Ownable {
     function processSubmission(bytes32 id, Core.SubmissionStatus response) public onlyOwner {
         // TODO - Add challenge deadline logic
         submissions[id].metaData.status = response;
+    }
+
+    function getSubmission(address beneficiary, bytes32 storageID) public returns (Core.SubmissionMetaData memory) {
+        bytes32 id = sha256(abi.encodePacked(beneficiary, storageID));
+
+        if (!submissions[id].isValue) { // Submission doesn't exist for key pair
+            revert("Submission does not exist for provided beneficiary x storage ID");
+        }
+
+        return submissions[id].metaData;
     }
 
     // TODO - Implement fund recovery / fallback functionality
