367712b8e118fc0aa1e59db1fcff09415b465d43
epociask <empociask@dons.usfca.edu>
2023-01-23 01:16:12 -0800

    Token Contract

--- contracts/contracts/blocksec-dao/Token.sol
+++ contracts/contracts/blocksec-dao/Token.sol
@@ -2,12 +2,37 @@
 
 pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-contract BlackList
 
-contract BlockSecToken is ERC20 {
-    
-    constructor(uint256 initialSupply) ERC20("BlockSec", "BLKSEC") {
+library Helpers {
+
+    struct StakeEntry {
+        uint256 amount;
+        uint256 unlockTime;
+        address bounty;
+    }
+}
+
+contract BlockSecToken is ERC20, Ownable {
+    uint8 constant maxAccountLocks = 10;
+    uint256 stakeMinimum;
+
+    constructor(uint256 initialSupply, uint256 _stakeMinimum, address owner) ERC20("BlockSec", "BLKSEC") {
         _mint(msg.sender, initialSupply);
+        _changeOwner(owner);
+        
+        stakeMinimum = _stakeMinimum;
+    }
+
+    function stakeForBounty(address account, uint256 amount, address bounty) onlyOwner {
+        // TODO - Call bounty contract to ensure amount is above set stake threshold
+
+        require(balances[msg.sender] >= amount, "Caller must have enough");
+        
+        balances[msg.sender] -= amount;
+
+        LockEntry entry = LockEntry(amount, unlockTimestamp, bounty);
+
     }
 }
